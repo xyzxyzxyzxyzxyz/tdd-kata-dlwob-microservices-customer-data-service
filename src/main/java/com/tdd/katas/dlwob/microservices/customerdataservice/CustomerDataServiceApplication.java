500fca52bb53b4357cf7cc6ba093bc2127670d80
XYZ <idm.caz@mailinator.com>
2017-05-23 11:40:36 +0200

    Initial commit

---
+++ src/main/java/com/tdd/katas/dlwob/microservices/customerdataservice/CustomerDataServiceApplication.java
@@ -0,0 +1,12 @@
+package com.tdd.katas.dlwob.microservices.customerdataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerDataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerDataServiceApplication.class, args);
+	}
+}
